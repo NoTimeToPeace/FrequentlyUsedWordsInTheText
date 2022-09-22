30da0a2d580dfea9857292b5a9d2ba4bc4faccc9
NoTimeToPeace <NoTimeToPeace>
2022-09-22 22:32:26 +0300

    Added analyze count words

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,6 +1,58 @@
 #include "analyzer.h"
+#include <algorithm>
+#include <QtAlgorithms>
+
+QVector<QPair<QString, int>> Analyzer::sortByValue()
+{
+    QVector<QPair<QString, int>> vec;
+
+    for (auto it = m_key_words.cbegin(); it != m_key_words.cend(); ++it)
+    {
+      vec.push_back(qMakePair(it.key(), it.value()));
+    }
+    auto sortByVal=[](const QPair<QString, int> &a,
+                   const QPair<QString, int> &b)
+    {
+        return (a.second > b.second);
+    };
+    std::sort(vec.begin(), vec.end(), sortByVal);
+
+    return vec;
+}
 
 Analyzer::Analyzer()
 {
+    qDebug() << "Initial " << this;
+}
 
+Analyzer::Analyzer(QString text) : m_text(text.toLower())
+{
+    qDebug() << "Initial " << this;
+}
+
+Analyzer::~Analyzer()
+{
+    qDebug() << "Destroyed " << this;
+}
+
+void Analyzer::removePunctuationMarks()
+{
+    m_text.removeIf([](QChar x){return ispunct(x.toLatin1()) || x.isDigit();});
+    m_text = m_text.simplified();
+}
+
+void Analyzer::wordsCount()
+{
+    QStringList splitWords = m_text.split(" ");
+    for (QString& val:splitWords) {
+        if (m_key_words.find(val) == m_key_words.end()) {
+            m_key_words.insert(val, m_text.count(val));
+        }
+    }
+    m_sorted_count_words = sortByValue();
+}
+
+const QVector<QPair<QString, int>> Analyzer::getWordsCount()
+{
+    return m_sorted_count_words;
 }
--- analyzer.h
+++ analyzer.h
@@ -1,10 +1,33 @@
 #ifndef ANALYZER_H
 #define ANALYZER_H
 
+#include <QString>
+#include <QMap>
+#include <QDebug>
+
 class Analyzer
 {
+private:
+    QString m_text; // Analyzed text
+    QMap<QString, int> m_key_words; // Rep Counting
+    QVector<QPair<QString, int>> m_sorted_count_words; // Отсортированное количество слов
+
+private:
+    QVector<QPair<QString, int>> sortByValue(); // Сортировка Map по значению
+
 public:
     Analyzer();
+    Analyzer(QString text);
+    Analyzer(const Analyzer& val) = delete;
+    Analyzer(Analyzer&& val) = delete;
+    Analyzer operator=(const Analyzer&) = delete;
+    Analyzer operator=(Analyzer&&) = delete;
+    ~Analyzer();
+
+public:
+    void removePunctuationMarks(); // Удаление всех пунктационных знаков и переходов на новую строку
+    void wordsCount(); // Подсчёт повторяющихся слов
+    const QVector<QPair<QString, int>> getWordsCount(); // Возврат отсортированных подсчитанных слов
 };
 
 #endif // ANALYZER_H
